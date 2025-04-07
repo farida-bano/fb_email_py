2a732251903ab2b3a155d2485f1bbc0121d53f60
Farida Bano <159705091+farida-bano@users.noreply.github.com>
2025-04-07 11:15:22 +0500

    send_email.py

--- send_email.py
+++ send_email.py
@@ -4,8 +4,6 @@ import schedule
 import time
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
 import ssl
 import threading
 
